202a20e4fb44b999e673801deb188ed15519f66f
wfswee <wfswee@chicagobooth.edu>
2021-04-15 06:54:36 +0000

    updated form new to push params into nested hash

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -30,8 +30,8 @@ class MoviesController < ApplicationController
 
   def create
     @movie = Movie.new
-    @movie.title = params.fetch(:title)
-    @movie.description = params.fetch(:description)
+    @movie.title = params.fetch(:movie).fetch(:title)
+    @movie.description = params.fetch(:movie).fetch(:description)
 
     if @movie.valid?
       @movie.save
