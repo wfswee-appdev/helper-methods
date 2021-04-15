083f4a8d7ebc043f3063724a79fbf4c71f6fa20e
wfswee <wfswee@chicagobooth.edu>
2021-04-15 03:43:16 +0000

    dropped as many render statements as possible

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,7 @@ class MoviesController < ApplicationController
   def new
     @the_movie = Movie.new
 
-    render template: "movies/new.html.erb"
+   # render template: "movies/new" # we can leave off "movies/new" because movies matches the controller name and new matches method name...we can also just remove this entirely because of those matches
   end
 
   def index
@@ -15,9 +15,9 @@ class MoviesController < ApplicationController
         render json: @list_of_movies
       end
 
-      format.html do
-        render template: "movies/index.html.erb"
-      end
+      format.html #do
+      #  render template: "movies/index"
+      #end
     end
   end
 
@@ -28,7 +28,7 @@ class MoviesController < ApplicationController
 
     @the_movie = matching_movies.first
 
-    render template: "movies/show.html.erb"
+    # render template: "movies/show"
   end
 
   def create
@@ -40,7 +40,7 @@ class MoviesController < ApplicationController
       @the_movie.save
       redirect_to(movies_url, notice: "Movie created successfully." )
     else
-      render template: "movies/new.html.erb"
+      render "new" # we dropped movies/ here because it matches the contorller
     end
   end
 
@@ -50,8 +50,6 @@ class MoviesController < ApplicationController
     matching_movies = Movie.where({ :id => the_id })
 
     @the_movie = matching_movies.first
-
-    render template: "movies/edit.html.erb"
   end
 
   def update
@@ -63,9 +61,9 @@ class MoviesController < ApplicationController
 
     if the_movie.valid?
       the_movie.save
-      redirect_to(movie_url(the_movie), notice: "Movie updated successfully." )
+      redirect_to movie_url(the_movie), notice: "Movie updated successfully."
     else
-      redirect_to(movie_url(themovie), alert: "Movie failed to update successfully." )
+      redirect_to movie_url(themovie), alert: "Movie failed to update successfully."
     end
   end
 
@@ -75,6 +73,6 @@ class MoviesController < ApplicationController
 
     the_movie.destroy
 
-    redirect_to(movies_url, notice: "Movie deleted successfully." )
+    redirect_to movies_url, notice: "Movie deleted successfully."
   end
 end
