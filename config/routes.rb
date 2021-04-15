4083bb151079d8b63c01d54e58a3c5b6f4e1be60
wfswee <wfswee@chicagobooth.edu>
2021-04-15 02:58:04 +0000

    gave names to routes and started using them

--- config/routes.rb
+++ config/routes.rb
@@ -6,29 +6,18 @@ Rails.application.routes.draw do
   # Routes for the Movie resource:
 
   # CREATE
- # post "/movies", controller: "movies", action: "create"
-  post "/movies" => "movies#create"
-
- # get "/movies/new", controller: "movies", action: "new"
-  get "/movies/new" => "movies#new"
+  post "/movies" => "movies#create", as: :movies # movies_url and movies_path
+  get "/movies/new" => "movies#new", as: :new_movie # new_movie_url and new_movie_path
           
   # READ
- # get "/movies", controller: "movies", action: "index"
   get "/movies" => "movies#index"
-
- # get "/movies/:id", controller: "movies", action: "show"
-  get "/movies/:id" => "movies#show"
+  get "/movies/:id" => "movies#show", as: :movie # movie_path() (expects an argument to populate :id segment)
   
   # UPDATE
-  # patch "/movies/:id", controller: "movies", action: "update"
   patch "/movies/:id" => "movies#update"
-  
- # get "/movies/:id/edit", controller: "movies", action: "edit"
-  get "/movies/:id/edit" => "movies#edit"
+  get "/movies/:id/edit" => "movies#edit", as: :edit_movie # # edit_movie_path() (expects an argument to populate :id segment)
 
   # DELETE
- # delete "/movies/:id", controller: "movies", action: "destroy"
-
   delete "/movies/:id" => "movies#destroy"
 
   #------------------------------
